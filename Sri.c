cc4d3b260531ad7d0d208e8890c65880ba2e1579
Srikanth Thimmapuram <147969870+555-Trigger@users.noreply.github.com>
2026-07-09 10:55:16 +0530

    Correct main function and printf statements
    Fix syntax errors and improve formatting.

--- Sri.c
+++ Sri.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<math.h>
 
-voidmain(){
+int main(void)
+{
 
-int a,b;
-a=121;
-b=49;
-printf(Square root of a=%f\n;)
-printf(Square root of b=%f\n;)
+      int a,b;
+      a=121;
+      b=49;
+      printf("Square root of a=%f\n");
+      printf("Square root of b=%f\n");
 
 }
